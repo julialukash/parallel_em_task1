d74f8162132e049dcc5bbe2f28052d8f14991c90
julialukash <julialukashkina@gmail.com>
2016-09-28 15:54:33 +0300

    inverse changed to brute-force for 2x2

--- src/inverse.h
+++ src/inverse.h
@@ -1,38 +1,27 @@
-// C++ matrix inversion (boost::ublas)
-// Another recurrent question is “how do I invert a matrix in [my library of choice]?”. Neither boost::ublas nor MTL documentation answers this satisfactorily.
-// If you’re using boost::ublas, you can find the InvertMatrix funcion here: http://www.crystalclearsoftware.com/cgi-bin/boost_wiki/wiki.pl?LU_Matrix_Inversion
-// And a simple example below:
-
 #include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/lu.hpp>
-#include <boost/numeric/ublas/io.hpp>
 
 using namespace boost::numeric::ublas;
+
 using namespace std;
 
- /* Matrix inversion routine.
- Uses lu_factorize and lu_substitute in uBLAS to invert a matrix */
 template<class T>
-bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse)
+int InvertMatrix(const matrix<T>& input, matrix<T>& inverse)
 {
-	typedef permutation_matrix<std::size_t> pmatrix;
+    if (input.size1() != 2 || input.size2() != 2)
+	{
+        std::cerr << "Matrix's size is incorrect\n";
+        	exit(1);
+	}
 
-	// create a working copy of the input
-	matrix<T> A(input);
+    auto det = input(0, 0) * input(1, 1) - input(0, 1) * input(1, 0);
+    if (det != 0)
+    {
+        inverse = matrix<T>(input.size1(), input.size2());
+        inverse(0, 0) = input(1, 1);
+        inverse(1, 1) = input(0, 0);
+        inverse(0, 1) = -input(0, 1);
+        inverse(1, 0) = -input(1, 0);
+    }
 
-	// create a permutation matrix for the LU-factorization
-	pmatrix pm(A.size1());
-
-	// perform LU-factorization
-	int res = lu_factorize(A, pm);
-	if (res != 0)
-		return false;
-
-	// create identity matrix of "inverse"
-	inverse.assign(identity_matrix<T> (A.size1()));
-
-	// backsubstitute to get the inverse
-	lu_substitute(A, pm, inverse);
-
-	return true;
+	return det;
 }
